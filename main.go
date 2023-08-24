992da5794f4a0de89e4b5e999716685cdfd9c292
Max Morgan <maxmmorgan93@gmail.com>
2023-08-24 10:54:04 -0500

    arrays, slices, and for loops in go. Created ticket booking slice and created loop to run while counter under slice length

--- main.go
+++ main.go
@@ -14,12 +14,47 @@ func main(){
 	fmt.Printf("Total tickets available: %v & Remaining tickets available: %v \n",totalTickets,remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var bookings []string
+	
+
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 	// ask user for name & tickets
 
-	userName = "Tom"
-	fmt.Printf("%v has booked %v tickets\n", userName, userTickets)
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	
+	
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter the number of tickets you would like to purchase: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName + " " + lastName)
+
+	for i := 0; i < len(bookings); i++ {
+	fmt.Printf("The whole array: %v \n", bookings)
+	fmt.Printf("Index 0: %v \n", bookings[i])
+	fmt.Printf("The array type is: %T \n", bookings)
+	fmt.Printf("The length of the bookings array is %v \n", len(bookings))
+	
+	fmt.Printf("User %v has booked %v tickets\n", bookings[i], userTickets)
+	fmt.Printf("Thanks for your order %v, you will receive %v tickets in your email: %v\n", bookings[i], userTickets, email)
+	fmt.Printf("Total tickets remaining %v\n", remainingTickets)
+	}
+
+	
+	
+	
 
 	
 }
