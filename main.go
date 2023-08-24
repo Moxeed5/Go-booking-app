573d3f236f1950521119b3e464d0e80d86113917
Max Morgan <maxmmorgan93@gmail.com>
2023-08-24 11:40:50 -0500

    learning loops. for loop and go version of foreach.

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
 
 func main(){
 	conferenceName := "Go Conference" //cannot explicitly declare type when using := and can only be used with a func. 
 	const conferenceTickets int = 50
 	var totalTickets = 50
 	var remainingTickets uint = 50
+	bookings := []string{}
 	
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -14,14 +19,16 @@ func main(){
 	fmt.Printf("Total tickets available: %v & Remaining tickets available: %v \n",totalTickets,remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var bookings []string
+	
 	
 
-
+	for {
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
+	
+	
 	// ask user for name & tickets
 
 	fmt.Println("Enter your first name: ")
@@ -41,21 +48,34 @@ func main(){
 	remainingTickets = remainingTickets - userTickets
 	bookings = append(bookings, firstName + " " + lastName)
 
-	for i := 0; i < len(bookings); i++ {
-	fmt.Printf("The whole array: %v \n", bookings)
-	fmt.Printf("Index 0: %v \n", bookings[i])
-	fmt.Printf("The array type is: %T \n", bookings)
-	fmt.Printf("The length of the bookings array is %v \n", len(bookings))
 	
-	fmt.Printf("User %v has booked %v tickets\n", bookings[i], userTickets)
-	fmt.Printf("Thanks for your order %v, you will receive %v tickets in your email: %v\n", bookings[i], userTickets, email)
-	fmt.Printf("Total tickets remaining %v\n", remainingTickets)
+	// fmt.Printf("The whole array: %v \n", bookings)
+	// fmt.Printf("Index 0: %v \n", bookings[0])
+	// fmt.Printf("The array type is: %T \n", bookings)
+	// fmt.Printf("The length of the bookings array is %v \n", len(bookings))
+	
+	fmt.Printf("User %v has booked %v tickets\n", bookings[0], userTickets)
+	fmt.Printf("Thanks for your order %v, you will receive %v tickets in your email: %v\n", bookings[0], userTickets, email)
+
+	firstNames := []string{}
+
+	//the below loop is a foreach. Foreach index within bookings slice (declared above & not an array bc we don't specify length).
+	//booking is the temp var within the foreach. Similar to var item in List in c#
+	//range is used to return element within the index and store it in booking var
+	//fields is a method within strings package. Fields separates elements within a single element on the space. 
+	//****update**** replaced index with underscore which tells go we are not using the variable. 
+	//if we need to use the index of the slice, use index. If you just need the element, use an underscore in place of index. 
+	//I can think of scenerios where every xth element needs to be manipulated. In that case you'd want to use index to track position.
+	for _, booking := range bookings{ 
+
+		var names = strings.Fields(booking) 
+		firstNames = append(firstNames, names[0])
+		fmt.Printf("%T", names)
+	} 
+	fmt.Printf("First names of all current bookings %v\n", firstNames)
+	
 	}
-
 	
-	
-	
-
 	
 }
 
